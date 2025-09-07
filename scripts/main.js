bbc8ffc9a174b7588d5d4d697141ca65151e51b1
Hanna <hannahazratian@gmail.com>
2025-09-08 00:57:46 +0330

    feat(scripts/utils.js): add setDate and toggleTheme functions and import in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,22 @@
 import { initializeSidebar } from "./sidebar-mobile.js";
+import { setDate } from "./utils.js";
+import { toggleTheme } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // TODO: Render Tasks
   // TODO: Create Task
   // TODO: Update Task
   // TODO: Delete Task
-  // TODO: Set Date
-  // TODO: Toggle mode
+
+  // Set Date
+  const setDates = document.querySelectorAll(".date");
+  setDates.forEach((date) => {
+    date.textContent += setDate();
+  });
 
   // Sidebar
   initializeSidebar();
+
+  // Toggle mode
+  toggleTheme();
 });
