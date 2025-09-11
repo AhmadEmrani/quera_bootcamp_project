fa800259893e0b2e5fe067e3163e54f20668dfdb
AhmadEmrani <mrany393@gmail.com>
2025-09-11 22:25:43 +0330

    feat(render function): update render for editing

--- scripts/render-tasks.js
+++ scripts/render-tasks.js
@@ -1,8 +1,10 @@
-import { tasks } from "./main.js";
+// import { tasks } from "./main.js";
 
 const tasksContainer = document.querySelector("#tasks-container");
+const completedTasks = document.querySelector(".completed-tasks");
 
-export function renderAllTasks() {
+export function renderAllTasks(tasks) {
+  console.log(tasks);
   tasksContainer.innerHTML = "";
 
   tasks.forEach((todo) => {
@@ -65,7 +67,7 @@ export function renderAllTasks() {
       `;
 
       tasksContainer.appendChild(li);
-    } else if (todo.isCompleted) {
+    } if (todo.isCompleted) {
       const li = document.createElement("li");
       li.className =
         "flex flex-row justify-between pl-4 py-4 border-solid border-neutral-light-300 border-[1px] rounded-[12px]";
@@ -84,7 +86,7 @@ export function renderAllTasks() {
           <h3
             class="text-sm md:text-base font-[600] text-neutral-light-700 line-through"
           >
-            خرید میوه و سبزیجات و لبنیات برای خانه
+          ${todo.title}
           </h3>
         </div>
       </div>
@@ -94,6 +96,74 @@ export function renderAllTasks() {
     </figure>
       
       `;
+      completedTasks.appendChild(li);
+    } if (todo.isediting) {
+      const div = document.createElement("div");
+      div.className =
+        "font-yekan bg-background-light border border-solid border-neutral-light-200 rounded-xl shadow-app";
+      div.innerHTML = `
+      <div
+      class="flex flex-col items-start gap-6 p-4 pb-6 border-b border-b-neutral-light-200"
+    >
+      <div class="flex flex-col gap-2">
+        <h3
+          class="font-semibold text-sm text-neutral-light-800 md:font-bold md:text-base"
+        >
+        ${todo.title}
+        </h3>
+        <p
+          class="font-normal text-xs text-neutral-light-600 md:text-sm"
+        >
+        ${todo.description}
+        </p>
+      </div>
+      <div class="">
+        <button
+          class="font-semibold text-xs text-[#11A483] bg-[#C3FFF1] px-2 py-1.5 flex items-center gap-1.5 rounded-[4px] md:text-sm"
+        >
+          <img
+            src="../assets/icons/close.svg"
+            alt="حذف"
+            class="w-4 h-4 md:w-5 md:h-5"
+          />
+          پایین
+        </button>
+      </div>
+      <div class="hidden">
+        <button
+          class="font-semibold text-xs text-[#FFAF37] bg-[#FFEFD6] px-2 py-1.5 flex items-center gap-1.5 rounded-[4px] md:text-sm"
+        >
+          <img
+            src="../assets/icons/close.svg"
+            alt="حذف"
+            class="w-4 h-4 md:w-5 md:h-5"
+          />
+          متوسط
+        </button>
+      </div>
+      <div class="hidden">
+        <button
+          class="font-semibold text-xs text-[#FF5F37] bg-[#FFE2DB] px-2 py-1.5 flex items-center gap-1.5 rounded-[4px] md:text-sm"
+        >
+          <img
+            src="../assets/icons/close.svg"
+            alt="حذف"
+            class="w-4 h-4 md:w-5 md:h-5"
+          />
+          بالا
+        </button>
+      </div>
+    </div>
+    <div class="p-4 flex justify-end md:pt-5">
+      <button
+        class="font-semibold text-xs text-on-primary-light bg-app-primary-light flex items-center justify-center px-4 py-1.5 rounded-md md:text-sm"
+      >
+        ویرایش تسک
+      </button>
+    </div>
+      
+      `;
+      tasksContainer.appendChild(div);
     }
   });
 }
