5ef7251d75bf8c99208bfc723a1cd7e8a2784fe2
Hanna <hannahazratian@gmail.com>
2025-09-13 17:17:22 +0330

    feat(scripts/render-tasks.js): add li.dataset.id

--- scripts/render-tasks.js
+++ scripts/render-tasks.js
@@ -1,5 +1,3 @@
-// import { tasks } from "./main.js";
-
 const tasksContainer = document.querySelector("#tasks-container");
 const completedTasks = document.querySelector(".completed-tasks");
 
@@ -68,7 +66,7 @@ export function renderAllTasks(tasks) {
     if (!todo.isCompleted) {
       const li = document.createElement("li");
       li.className =
-        "flex flex-row justify-between pl-4 py-4 border border-neutral-light-200 rounded-[12px] relative mb-4";
+        "task-card flex flex-row justify-between pl-4 py-4 border border-neutral-light-200 rounded-[12px] relative mb-4";
 
       li.innerHTML = `
         <!-- Edit/Delete buttons -->
@@ -95,13 +93,15 @@ export function renderAllTasks(tasks) {
             <p class="text-[12px] font-[400] text-neutral-light-500 md:text-[14px]">${todo.description}</p>
           </div>
         </div>
-        <button class="more-btn cursor-pointer">
+        <figure class="more-btn cursor-pointer">
           <img src="../assets/icons/3dotIcon.svg" alt="نمایش بیشتر" />
-        </button>
+        </figure>
       `;
 
+      li.dataset.id = todo.id;
       tasksContainer.appendChild(li);
-    } if (todo.isCompleted) {
+    }
+    if (todo.isCompleted) {
       const li = document.createElement("li");
       li.className =
         "flex flex-row justify-between pl-4 py-4 border-solid border-neutral-light-300 border-[1px] rounded-[12px]";
@@ -130,74 +130,8 @@ export function renderAllTasks(tasks) {
     </figure>
       
       `;
+      li.dataset.id = todo.id;
       completedTasks.appendChild(li);
-    } if (todo.isediting) {
-      const div = document.createElement("div");
-      div.className =
-        "font-yekan bg-background-light border border-solid border-neutral-light-200 rounded-xl shadow-app";
-      div.innerHTML = `
-      <div
-      class="flex flex-col items-start gap-6 p-4 pb-6 border-b border-b-neutral-light-200"
-    >
-      <div class="flex flex-col gap-2">
-        <h3
-          class="font-semibold text-sm text-neutral-light-800 md:font-bold md:text-base"
-        >
-        ${todo.title}
-        </h3>
-        <p
-          class="font-normal text-xs text-neutral-light-600 md:text-sm"
-        >
-        ${todo.description}
-        </p>
-      </div>
-      <div class="">
-        <button
-          class="font-semibold text-xs text-[#11A483] bg-[#C3FFF1] px-2 py-1.5 flex items-center gap-1.5 rounded-[4px] md:text-sm"
-        >
-          <img
-            src="../assets/icons/close.svg"
-            alt="حذف"
-            class="w-4 h-4 md:w-5 md:h-5"
-          />
-          پایین
-        </button>
-      </div>
-      <div class="hidden">
-        <button
-          class="font-semibold text-xs text-[#FFAF37] bg-[#FFEFD6] px-2 py-1.5 flex items-center gap-1.5 rounded-[4px] md:text-sm"
-        >
-          <img
-            src="../assets/icons/close.svg"
-            alt="حذف"
-            class="w-4 h-4 md:w-5 md:h-5"
-          />
-          متوسط
-        </button>
-      </div>
-      <div class="hidden">
-        <button
-          class="font-semibold text-xs text-[#FF5F37] bg-[#FFE2DB] px-2 py-1.5 flex items-center gap-1.5 rounded-[4px] md:text-sm"
-        >
-          <img
-            src="../assets/icons/close.svg"
-            alt="حذف"
-            class="w-4 h-4 md:w-5 md:h-5"
-          />
-          بالا
-        </button>
-      </div>
-    </div>
-    <div class="p-4 flex justify-end md:pt-5">
-      <button
-        class="font-semibold text-xs text-on-primary-light bg-app-primary-light flex items-center justify-center px-4 py-1.5 rounded-md md:text-sm"
-      >
-        ویرایش تسک
-      </button>
-    </div>
-      
-      `;
-      tasksContainer.appendChild(div);
     }
   });
 }
