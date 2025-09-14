d20df9a98c808aadeb49238c87ace61a0571d16c
Hamed <vafaei.hamed@gmail.com>
2025-09-14 17:17:15 +0330

    refactor(allProject):refactor colors and styles

--- scripts/complete-task.js
+++ scripts/complete-task.js
@@ -1,16 +1,30 @@
 export function completeTask(onCompleted) {
   const tasksList = document.getElementById("tasks-container");
+  const taskCompletedList = document.getElementById("complete-tasks-container");
 
   tasksList.addEventListener("click", (e) => {
     const element = e.target;
-    const checkBoxInput = element.closest("#remember-add");
+    const checkBoxInput = element.closest(".remember-add");
 
     if (!checkBoxInput) return;
 
-    const taskCard = deleteBtn.closest(".task-card");
+    const taskCard = checkBoxInput.closest(".task-card");
     if (!taskCard) return;
 
     const taskId = taskCard.dataset.id;
+    onCompleted(taskId);
+  });
+
+  taskCompletedList.addEventListener("click", (e) => {
+    const element = e.target;
+    const checkBoxInput = element.closest(".remember-add-done");
+
+    if (!checkBoxInput) return;
+
+    const taskCardComplete = checkBoxInput.closest(".task-card-complete");
+    if (!taskCardComplete) return;
+
+    const taskId = taskCardComplete.dataset.id;
     onCompleted(taskId);
   });
 }
